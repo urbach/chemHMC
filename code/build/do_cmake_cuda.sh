feac51e1eabf5835ca02a07b41f90bffcef725a3
Marcogarofalo <marcogarofalo31@gmail.com>
2022-11-24 16:40:15 +0100

    configurations files

--- code/build/do_cmake_cuda.sh
+++ code/build/do_cmake_cuda.sh
@@ -13,8 +13,10 @@ cmake .. \
   -DCMAKE_BUILD_TYPE=DEBUG \
   -DKokkos_ENABLE_OPENMP=OFF \
   -DKokkos_ENABLE_CUDA=ON \
+  -DKokkos_CXX_STANDARD=17 \
   -DKokkos_ENABLE_SERIAL=ON  \
   -DKokkos_ARCH_KEPLER35=ON \
   -DKokkos_ENABLE_CUDA_LAMBDA=ON \
+  -DCMAKE_PREFIX_PATH="/hiskp4/garofalo/yaml-cpp/install_dir" \
   -DCMAKE_CXX_COMPILER=${projectHOME}/external/kokkos/bin/nvcc_wrapper 
 
