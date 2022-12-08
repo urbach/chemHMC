79e653ffb63e9b8d6cdd4470445c0a33c64d2e0e
Vahideh Alizadeh <vahideh.90.alizadeh@gmail.com>
2022-12-08 15:59:45 +0100

    overleaf-d

--- code/build/do_cmake_openMP.sh
+++ code/build/do_cmake_openMP.sh
@@ -11,6 +11,6 @@ cmake .. \
   -DKokkos_ENABLE_CUDA=OFF \
   -DKokkos_ENABLE_SERIAL=ON \
   -DKokkos_CXX_STANDARD=14  \
-  -DCMAKE_PREFIX_PATH="/home/garofalo/programs/yaml-cpp/install_dir"  
+  -DCMAKE_PREFIX_PATH="/home/alizadeh/programs/yaml-cpp/install_dir"  
 
  
