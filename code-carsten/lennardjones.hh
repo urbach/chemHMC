4b1c5d2820408a7973f30834b7786a43d7b15b07
Carsten Urbach <curbach@gmx.de>
2023-01-13 21:20:45 +0100

    ...

---
+++ code-carsten/lennardjones.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include<math>
+
+inline double LennardJones(const double r, const int n=12,
+                           const double a=1, const double b=1) {
+  
+  return(a/pow(r, n) - b/pow(r, 6));
+}
