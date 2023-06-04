4fb5bb3e7f27434cb4d08898a49ff3a1e112a2c1
V-Alizade <vahideh.90.alizadeh@gmail.com>
2023-06-04 16:02:21 +0200

    read_infile debuge

--- code/modules/read_infile.cpp
+++ code/modules/read_infile.cpp
@@ -12,19 +12,24 @@
 #include "global.hpp"
 #include "yaml-cpp/yaml.h"
 #include "particles.hpp"
+#include "fmt/core.h"
 
 
 
 template<class T>
 T check_and_assign_value(YAML::Node doc, const char* tag) {
     if (!doc[tag]) {
-        printf("error: tag %s is not present in the input file\n", tag);
+        fmt::print("error: tag '{}' is not present in the input file\n", tag);
         std::cout << "infile structure:" << std::endl;
         std::cout << doc << std::endl;
         Kokkos::abort("params not found");
     }
 
-    return doc[tag].as<T>();
+    try {
+        return doc[tag].as<T>();
+    } catch (YAML::TypedBadConversion<T>) {
+        Kokkos::abort(fmt::format ("Incorrect input type for '{}'.", tag).c_str());
+    }
 }
 template double check_and_assign_value<double>(YAML::Node, const char*);
 template int check_and_assign_value<int>(YAML::Node, const char*);
@@ -55,22 +60,20 @@ YAML::Node read_params(int argc, char** argv) {
     int opt = -1;
     YAML::Node doc;
     // search for command line option and put filename in "infilename"
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "-i") == 0) {
             opt = i + 1;
             break;
         }
     }
-    if (opt < 0) {
+    if (opt < 0 || opt == argc) {
         std::cout << "No input file specified, Aborting" << std::endl;
         std::cout << "usage:  ./main -i infile.in" << std::endl;
-        exit(1);
+        exit(1); // TODO: call Kokkos::finalize
     }
-    else {
-        std::string infilename = argv[opt];
-        std::cout << "Trying input file " << infilename << std::endl;
+    std::string infilename = argv[opt];
+    std::cout << "Trying input file " << infilename << std::endl;
 
-        doc = YAML::LoadFile(infilename);
-    }
+    doc = YAML::LoadFile(infilename);
     return doc;
 }
