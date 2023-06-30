8bf8068d0575fdec5b7123b31ca96a78f4baa880
Marcogarofalo <marcogarofalo31@gmail.com>
2023-06-30 14:51:42 +0200

    commenting out writing of the device rng

--- code/modules/particles.cpp
+++ code/modules/particles.cpp
@@ -36,18 +36,18 @@ particles_type::particles_type(YAML::Node doc) : params(doc) {
 }
 
 void particles_type::save_device_rng() {
-    rand_pool.return_rng_state(hs);
-    FILE* f;
-    f = fopen(params.rng_device_state.c_str(), "w+");
-    printf("Saving rnd device...  %d %d\n",N,padding);
-    int i=fwrite(&hs(0, 0), sizeof(uint64_t), N * padding, f);
-    fclose(f);
+    // rand_pool.return_rng_state(hs);
+    // FILE* f;
+    // f = fopen(params.rng_device_state.c_str(), "w+");
+    // printf("Saving rnd device...  %d %d\n",N,padding);
+    // int i=fwrite(&hs(0, 0), sizeof(uint64_t), N * padding, f);
+    // fclose(f);
 }
 void particles_type::load_device_rng() {
-    FILE* f;
-    f = fopen(params.rng_device_state.c_str(), "r");
-    int i=fread(&hs(0, 0), sizeof(uint64_t), N * padding, f);
-    if (i!=N*padding) Kokkos::abort("invalid rng_device_state file\n");
-    rand_pool.load_rng_state(hs);
-    fclose(f);
+    // FILE* f;
+    // f = fopen(params.rng_device_state.c_str(), "r");
+    // int i=fread(&hs(0, 0), sizeof(uint64_t), N * padding, f);
+    // if (i!=N*padding) Kokkos::abort("invalid rng_device_state file\n");
+    // rand_pool.load_rng_state(hs);
+    // fclose(f);
 }
