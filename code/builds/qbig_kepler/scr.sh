f205521b0e369ff08b7877d7d7bd7bda43139386
Marcogarofalo <marcogarofalo31@gmail.com>
2023-07-14 12:47:04 +0200

    slurm script for kepler

---
+++ code/builds/qbig_kepler/scr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=04:00:00
+#SBATCH --qos=devel
+# #SBATCH --partition=devel
+#SBATCH --partition=batch
+# #SBATCH --reservation=eb_testing
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:kepler:1        # 2 gpus per node out of 8 ?
+#SBATCH --mem=15GB
+
+source load_modules_qbig_kepler.sh
+#export KOKKOS_PROFILE_LIBRARY=/hiskp4/garofalo/chemHMC/code/external/kokkos-tools/kp_memory_events.so
+# export KOKKOS_PROFILE_LIBRARY=/hiskp4/garofalo/chemHMC/code/external/kokkos-tools/kp_kernel_logger.so
+
+ 
+#rm rng* out_xyz.txt
+#/qbigwork/garofalo/valgrind/install_dir/bin/valgrind --leak-check=full ../../chemHMC/code/build/main//main -i input_I.yaml
+#../../chemHMC/code/build/main//main -i input_I.yaml
+main/main -i ../test.yaml
+                                         
