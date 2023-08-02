d842d5c61759416089113be6b62951d5f9ddc7c3
Marcogarofalo <marcogarofalo31@gmail.com>
2023-08-02 19:26:50 +0200

    cleaning

--- code/test/test.cpp
+++ code/test/test.cpp
@@ -1,14 +1,6 @@
-#include <limits>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <sys/time.h>
-
 #include <Kokkos_Core.hpp>
 
-
 class a_class {
-
 public:
     std::string sa;
     KOKKOS_FUNCTION void operator() (const int& i, double& s1) const { s1++; };
@@ -21,6 +13,7 @@ int main(int argc, char* argv[]) {
         a_class myclass;
         double sum;
         Kokkos::parallel_reduce("myclass", 1, myclass, sum);
+
     }
     Kokkos::finalize();
 }
