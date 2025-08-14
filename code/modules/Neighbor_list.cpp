e82adbb2d9b679a3941aa09584d85fb09357f90b
Petruchas <kai.buchmueller@gmx.de>
2025-08-14 23:01:38 +0200

    Small optimizations in Neighbor list builds

--- code/modules/Neighbor_list.cpp
+++ code/modules/Neighbor_list.cpp
@@ -24,7 +24,8 @@ void Neighbor_list::init_verlet_list(YAML::Node& doc, particles_instance& partic
     }
     if (coul_cutoff > lj_cutoff) neighbor_cutoff = coul_cutoff;
     else neighbor_cutoff = lj_cutoff;
-
+    // square the cutoff and add a skin distance
+    neighbor_cutoff_squared = neighbor_cutoff*neighbor_cutoff*1.2;
     verlet_list = Kokkos::View<int**>("verlet_list", N, max_neighbors);
     h_verlet_list = Kokkos::create_mirror_view(verlet_list);
 
@@ -49,44 +50,50 @@ void Neighbor_list::build_verlet_list(particles_instance& particles) {
     auto& neighbour_count = this->neighbour_count;
     auto& verlet_list = this->verlet_list;
     auto& L = particles.L;
+    auto& N = particles.N;
     auto& inverse_halved_L = particles.inverse_halved_L;
-    auto& cutoff_squared = particles.cutoff_squared;
+    auto& cutoff_squared = this->neighbor_cutoff_squared;
+
+    const double ihL0 = inverse_halved_L[0];
+    const double ihL1 = inverse_halved_L[1];
+    const double ihL2 = inverse_halved_L[2];
+    const double L0 = L[0], L1 = L[1], L2 = L[2];
+    const int neighbor_count_length = neighbour_count.extent(0);
 
     // Outer parallel_for
     Kokkos::parallel_for(
         "populate_verlet_list",
-        Kokkos::TeamPolicy<Tag_build_verlet_list>(x.extent(0), Kokkos::AUTO),
+        Kokkos::TeamPolicy<Tag_build_verlet_list>(N, Kokkos::AUTO),
         KOKKOS_LAMBDA(const Tag_build_verlet_list, const Kokkos::TeamPolicy<>::member_type& teamMember) {
             const int i = teamMember.league_rank();
+            const double xi0 = x(i,0), xi1 = x(i,1), xi2 = x(i,2);
 
             Kokkos::parallel_for(
-                Kokkos::TeamThreadRange(teamMember, neighbour_count.extent(0)),
+                Kokkos::TeamThreadRange(teamMember, i+1, N),
                 [&](const int j) {
-                    if (i < j) {
-                        double rx = x(i, 0) - x(j, 0);
-                        rx -= int(rx * inverse_halved_L[0]) * L[0];
-                        double r2 = rx * rx;
+                    double r_tmp = xi0 - x(j, 0);
+                    r_tmp -= int(r_tmp * ihL0) * L0;
+                    double r2 = r_tmp * r_tmp;
 
-                        double ry = x(i, 1) - x(j, 1);
-                        ry -= int(ry * inverse_halved_L[1]) * L[1];
-                        r2 += ry * ry;
+                    r_tmp = xi1 - x(j, 1);
+                    r_tmp -= int(r_tmp * ihL1) * L1;
+                    r2 += r_tmp * r_tmp;
 
-                        double rz = x(i, 2) - x(j, 2);
-                        rz -= int(rz * inverse_halved_L[2]) * L[2];
-                        r2 += rz * rz;
+                    r_tmp = xi2 - x(j, 2);
+                    r_tmp -= int(r_tmp * ihL2) * L2;
+                    r2 += r_tmp * r_tmp;
 
-                        if (r2 >= (cutoff_squared * 1.2)) return;
+                    if (r2 >= (cutoff_squared)) return;
 
-                        int current_count = Kokkos::atomic_fetch_add(&neighbour_count(i), 1);
-                        if (current_count < verlet_list.extent(1)) {
-                            verlet_list(i, current_count) = j;
-                        } else {
-                            Kokkos::printf("COUNT: %d\n", current_count);
-                            Kokkos::abort(
-                                "ERROR: Verlet list dimension not large enough for the number of neighbors! "
-                                "Increase it manually in the config file with MaxParticles."
-                            );
-                        }
+                    int current_count = Kokkos::atomic_fetch_add(&neighbour_count(i), 1);
+                    if (current_count < verlet_list.extent(1)) {
+                        verlet_list(i, current_count) = j;
+                    } else {
+                        Kokkos::printf("COUNT: %d\n", current_count);
+                        Kokkos::abort(
+                            "ERROR: Verlet list dimension not large enough for the number of neighbors! "
+                            "Increase it manually in the config file with MaxParticles."
+                        );
                     }
                 });
         });
