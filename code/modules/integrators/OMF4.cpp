1a9df6644d0181f393b6ffab0d01ceeaed7cb82c
MarkoMalezic <malezic15@gmail.com>
2026-06-22 13:36:09 +0200

    input file changes and update to the OMF4 integrator

--- code/modules/integrators/OMF4.cpp
+++ code/modules/integrators/OMF4.cpp
@@ -17,22 +17,38 @@ OMF4::OMF4(YAML::Node doc, params_class params) :
 void OMF4::integrate() {
 
     // initial half-step for the momenta
+    calc_manager->compute_force();
+    Kokkos::fence();
     particles->update_momenta(0.5 * eps[9]);
 
     // nsteps-1 full steps
     for (size_t i = 1; i < steps - 1; i++) {
         for (size_t j = 0; j < 5; j++) {
             particles->update_positions(eps[2 * j]);
-            particles->neighbor_list->build_verlet_list(*particles);
+            if (particles->neighbor_list_used) {
+                particles->neighbor_list->build_verlet_list(*particles);
+            }
+            calc_manager->compute_force();
+            Kokkos::fence();
             particles->update_momenta(eps[2 * j + 1]);
         }
     }
     // almost one more full step
     for (size_t j = 0; j < 4; j++) {
         particles->update_positions(eps[2 * j]);
+        if (particles->neighbor_list_used) {
+            particles->neighbor_list->build_verlet_list(*particles);
+        }
+        calc_manager->compute_force();
+        Kokkos::fence();
         particles->update_momenta(eps[2 * j + 1]);
     }
     particles->update_positions(eps[8]);
+    if (particles->neighbor_list_used) {
+        particles->neighbor_list->build_verlet_list(*particles);
+    }
     // final half-step in the momenta
+    calc_manager->compute_force();
+    Kokkos::fence();
     particles->update_momenta(0.5 * eps[9]);
 }
