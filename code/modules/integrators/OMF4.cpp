0b27d64f052f7dd5e7fea9627ad34033403dff8c
MarkoMalezic <malezic15@gmail.com>
2026-06-23 10:39:13 +0200

    fixed small merge mistake

--- code/modules/integrators/OMF4.cpp
+++ code/modules/integrators/OMF4.cpp
@@ -19,8 +19,6 @@ void OMF4::integrate() {
     // initial half-step for the momenta
     calc_manager->compute_force();
     Kokkos::fence();
-    calc_manager->compute_force();
-    Kokkos::fence();
     particles->update_momenta(0.5 * eps[9]);
 
     // nsteps-1 full steps
